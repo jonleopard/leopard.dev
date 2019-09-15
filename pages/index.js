e9f4ebaa7cf1484a364dba7d3148061c517012db
Jon Leopard <jontron2000@gmail.com>
2019-09-15 18:17:47 +0200

    Adding tailwind

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 export default function Index() {
   return (
-    <div>
-      <h1>Hello Next.js 👋</h1>
+    <div className="p-4 shadow rounded bg-white">
+      <h1 className="text-purple leading-normal">Next.js</h1>
+      <p className="text-grey-dark">with Tailwind CSS</p>
+
       <Link href="/about">
         <a>About</a>
       </Link>
