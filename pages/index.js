f64c4600fbd02f2d3940f2abf731122e5c94d071
Jon Leopard <jontron2000@gmail.com>
2019-09-16 21:47:56 +0200

    Added config. Spinning up a quick page

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,24 @@ import "../styles/index.css";
 
 export default function Index() {
   return (
-    <div className="p-4 shadow rounded bg-white">
-      <h1 className="text-purple leading-normal">Next.js</h1>
-      <p className="text-grey-dark">with Tailwind CSS</p>
-
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
+    <>
+      <div className="container mx-auto">
+        <div className="text-purple-600">
+          <h1>leopard.dev</h1>
+          <p className="text-grey-dark">
+            currently building great products @creationshop
+          </p>
+          <p>thanks for stopping by!</p>
+        </div>
+        <div className="text-green-500">
+          <p className="inline block">
+            Built by <a href="https://twitter.com/jonlprd">@jonlprd | </a>
+          </p>
+          <span>
+            <a href="https://jonleopard.com">jonleopard.com</a>
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
