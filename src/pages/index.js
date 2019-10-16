4f9f3a2076b3d72cd2a899b4a84758e2484877b5
Jon Leopard <jontron2000@gmail.com>
2019-10-16 21:43:38 +0200

    Little update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,16 @@ import "../styles/index.css";
 
 export default function Index() {
   return (
-    <>
-      <div className="container mx-auto">
-        <div className="text-purple-600">
-          <h1>leopard.dev</h1>
-          <p className="text-grey-dark">
-            currently building great products @creationshop
-          </p>
-          <p>thanks for stopping by!</p>
+    <div className="flex-col container mx-auto font-mono min-h-0">
+        <div className="text-sm text-gray-700">
+          <a href="https://leopard.dev">leopard.dev</a>
         </div>
-        <div className="text-green-500">
+        <div className="items-end">
+          <p className="text-gray-600 text-sm">
+            currently developing great products for @creationshop
+          </p>
+          <p className="text-gray-500 text-sm">thanks for stopping by!</p>
+        <div className="text-gray-400 text-xs">
           <p className="inline block">
             Built by <a href="https://twitter.com/jonlprd">@jonlprd | </a>
           </p>
@@ -21,6 +21,6 @@ export default function Index() {
           </span>
         </div>
       </div>
-    </>
+    </div>
   );
 }
